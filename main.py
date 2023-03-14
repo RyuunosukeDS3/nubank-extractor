ed231c58f19ddfd09565c5225a05b1eab5c96045
Matheus <matheusmiziara123@gmail.com>
2023-03-14 11:05:27 +0000

    Creating dockerfile

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from config import Config
 from scheduler import ScheduleJob
+import argparse
 
+parser = argparse.ArgumentParser()
 config = Config()
 config.logging_config()
 schedule_job = ScheduleJob()
-schedule_job.run_jobs()
+
+parser.add_argument(
+    "-sr",
+    "--single-run",
+    dest="single_run",
+    default=False,
+    help="Run extraction once",
+)
+args = parser.parse_args()
+
+if args.single_run:
+    schedule_job.extract_nubank_data()
+
+else:
+    schedule_job.run_jobs()
--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,5 +1,6 @@
 import schedule
 import time
+import logging
 from db_manager import NubankDbManager
 from nubank import NubankExtractor
 from config import Config
@@ -7,19 +8,30 @@ from config import Config
 
 class ScheduleJob(object):
     def __init__(self):
-        config = Config()
+        self.config = Config()
         nubank_db_manager = NubankDbManager()
         self.nubank_extractor = NubankExtractor(
-            config.user_id, config.password, config.cert_path, nubank_db_manager
+            self.config.user_id,
+            self.config.password,
+            self.config.cert_path,
+            nubank_db_manager,
         )
 
     def extract_nubank_data(self):
+        logging.info("Starting to run nubank extraction")
         self.nubank_extractor.get_and_navigate_through_card_statements()
         self.nubank_extractor.check_if_is_fully_paid()
         self.nubank_extractor.get_and_navigate_through_account_statements()
+        logging.info("Finished running extraction")
 
     def run_jobs(self):
-        schedule.every().day.at("20:52").do(self.extract_nubank_data)
+        logging.info(
+            "Scheduling job %s to run everyday at %s UTC",
+            self.extract_nubank_data.__name__,
+            self.config.run_time,
+        )
+
+        schedule.every().day.at(self.config.run_time).do(self.extract_nubank_data)
         while True:
             schedule.run_pending()
             time.sleep(60)
